2ccd1fcb4894913bbe7930e200f53cd3d7379c3a
MaurquiseBradford <bmaurquise04@gmail.com>
2024-08-19 18:54:07 -0500

    created index.js serving as api connecting to MongoDB.

---
+++ server/index.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+
+//Middleware
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+//define port
+const PORT = 3002
+
+
+
+//establish link to mongoDB Atlas Database
+mongoose.connect("mongodb+srv://mbradford1s:Ray24Lewis$$@cluster0.qwihs.mongodb.net/MERN_Health").then(() => console.log("Connected To Database"))  
+.then(()=>console.log("Backend Server Listening on PORT " + PORT))
+.then(() =>{
+    app.listen(PORT)
+}).catch((err) => console.log(err));
