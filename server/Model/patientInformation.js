daa369a5ccb354fdff6da415e6b1f2563f57967c
MaurquiseBradford <bmaurquise04@gmail.com>
2024-08-19 18:55:03 -0500

    created  mongoose schema.

---
+++ server/Model/patientInformation.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const patientSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  patient_id: {
+    type: Number,
+    required: true,
+  },
+  insurance: {
+    type: String,
+    required: true,
+  },
+
+});
+
+module.exports = mongoose.model("PatientInformation", patientSchema);
